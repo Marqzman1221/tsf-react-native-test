bb984b6e4869c1fda384d1b38901937ae6231eaf
tgmarqui <tgmarqui@ncsu.edu>
2021-02-26 12:45:33 -0500

    Updated metro config to handle tensorflow assets

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,19 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+  const { assetExts } = defaultConfig.resolver;
+
+  return {
+    resolver: {
+      // Add bin to assetExts
+      assetExts: [...assetExts, 'bin'],
+    }
+  }
+})();
+
+// module.exports = {
+//   transformer: {
+//     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+//   },
+// };
